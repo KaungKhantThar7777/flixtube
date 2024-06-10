8dffc921b05927d5790bcdd673e6a6a238d98e37
kaungkhantthar7777 <kaungkhantthar@rezeve.com>
2024-06-10 23:22:17 +0700

    added: http request in streaming

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,24 +1,37 @@
 const express = require("express");
-const fs = require("fs");
+const http = require("http");
 
-if (!process.env.PORT) {
-  throw new Error("PORT must be defined!!!");
-}
 const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = process.env.VIDEO_STORAGE_PORT;
+
+if (!PORT || !VIDEO_STORAGE_HOST || !VIDEO_STORAGE_PORT) {
+  throw new Error(
+    "PORT,VIDEO_STORAGE_HOST and VIDEO_STORAGE_PORT must be defined!!!"
+  );
+}
 
 const app = express();
 
 app.get("/video", async (req, res) => {
-  const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+  const videoPath = "/video?path=SampleVideo_1280x720_1mb.mp4";
 
-  const stats = await fs.promises.stat(videoPath);
+  const forwardRequest = http.request(
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: videoPath,
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardResponse) => {
+      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
 
-  res.writeHead(200, {
-    "Content-Length": stats.size,
-    "Content-Type": "video/mp4",
-  });
+      forwardResponse.pipe(res);
+    }
+  );
 
-  fs.createReadStream(videoPath).pipe(res);
+  req.pipe(forwardRequest);
 });
 
 app.listen(PORT, () => {
