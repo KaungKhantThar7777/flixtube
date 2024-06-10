c21b6277966cc489ef32995691c90ae6dd313388
kaungkhantthar7777 <kaungkhantthar@rezeve.com>
2024-06-10 23:21:45 +0700

    added: video-storage in compose

--- aws-storage/src/index.js
+++ aws-storage/src/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
-import { fromSSO } from "@aws-sdk/credential-provider-sso";
+import { fromSSO } from "@aws-sdk/credential-providers";
 
 const app = express();
 
 const PORT = process.env.PORT;
 const AWS_REGION = process.env.AWS_REGION;
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
 const AWS_SSO_PROFILE = process.env.AWS_SSO_PROFILE;
+
+if (!PORT || !AWS_REGION || !AWS_S3_BUCKET || !AWS_SSO_PROFILE) {
+  throw new Error(
+    "PORT,AWS_REGION,AWS_S3_BUCKET and AWS_SSO_PROFILE must be defined!!"
+  );
+}
 
 app.get("/video", async (req, res) => {
   const videoPath = req.query.path;
@@ -22,7 +28,7 @@ app.get("/video", async (req, res) => {
     });
     const { Body, ContentLength, ContentType } = await s3Client.send(
       new GetObjectCommand({
-        Bucket: S3_BUCKET_NAME,
+        Bucket: AWS_S3_BUCKET,
         Key: videoPath,
       })
     );
