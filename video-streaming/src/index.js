bdfc3944167d3de9206ed631abc4e5d33ca2df70
kaungkhantthar7777 <kaungkhantthar@rezeve.com>
2024-06-11 20:19:59 +0700

    added: mongo do for video metadata

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,39 +1,68 @@
 const express = require("express");
 const http = require("http");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = process.env.VIDEO_STORAGE_PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
 
-if (!PORT || !VIDEO_STORAGE_HOST || !VIDEO_STORAGE_PORT) {
-  throw new Error(
-    "PORT,VIDEO_STORAGE_HOST and VIDEO_STORAGE_PORT must be defined!!!"
-  );
+async function main() {
+  if (
+    !PORT ||
+    !VIDEO_STORAGE_HOST ||
+    !VIDEO_STORAGE_PORT ||
+    !DB_HOST ||
+    !DB_NAME
+  ) {
+    throw new Error(
+      "PORT,VIDEO_STORAGE_HOST,VIDEO_STORAGE_PORT, DB_HOST and DB_NAME must be defined!!!"
+    );
+  }
+
+  const client = await mongodb.MongoClient.connect(DB_HOST);
+  const db = client.db(DB_NAME);
+
+  const videosCollection = db.collection("videos");
+
+  const app = express();
+
+  app.get("/video", async (req, res) => {
+    const videoId = new mongodb.ObjectId(req.query.id);
+    console.log({ videoId });
+    const videoRecord = await videosCollection.findOne({ _id: videoId });
+
+    if (!videoRecord) {
+      res.sendStatus(404);
+
+      return;
+    }
+
+    const forwardRequest = http.request(
+      {
+        host: VIDEO_STORAGE_HOST,
+        port: VIDEO_STORAGE_PORT,
+        path: `/video?path=${videoRecord.videoPath}`,
+        method: "GET",
+        headers: req.headers,
+      },
+      (forwardResponse) => {
+        res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
+
+        forwardResponse.pipe(res);
+      }
+    );
+
+    req.pipe(forwardRequest);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} v2 update.`);
+  });
 }
 
-const app = express();
-
-app.get("/video", async (req, res) => {
-  const videoPath = "/video?path=SampleVideo_1280x720_1mb.mp4";
-
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: videoPath,
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardResponse) => {
-      res.writeHead(forwardResponse.statusCode, forwardResponse.headers);
-
-      forwardResponse.pipe(res);
-    }
-  );
-
-  req.pipe(forwardRequest);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} v2 update.`);
+main().catch((err) => {
+  console.error("Microservice failed to start");
+  console.error((err && err.stack) || err);
 });
