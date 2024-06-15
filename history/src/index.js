44dfe684a832f70b1258d1a23b738c2f34adc6cb
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2024-06-15 19:51:55 +0700

    feat: added direct message

--- history/src/index.js
+++ history/src/index.js
@@ -1,9 +1,45 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 const PORT = process.env.PORT;
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
+
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
+
+if (!process.env.DBHOST) {
+  throw new Error(
+    "Please specify the database host using environment variable DBHOST."
+  );
+}
+
+if (!process.env.DBNAME) {
+  throw new Error(
+    "Please specify the name of the database using environment variable DBNAME"
+  );
+}
 
 async function main() {
   const app = express();
+
+  app.use(express.json());
+  const client = await mongodb.MongoClient.connect(DBHOST);
+  const db = client.db(DBNAME);
+
+  const historyCollection = db.collection("history");
+
+  app.post("/viewed", async (req, res) => {
+    const videoPath = req.body.videoPath;
+
+    await historyCollection.insertOne({ videoPath });
+
+    console.log(`Added video ${videoPath} to history.`);
+    res.sendStatus(200);
+  });
 
   app.get("/", (req, res) => {
     res.send("hello world");
--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const http = require("http");
 const fs = require("fs");
 
 if (!process.env.PORT) {
@@ -7,20 +8,61 @@ if (!process.env.PORT) {
 const PORT = process.env.PORT;
 
 const app = express();
+let count = 0;
+function sendViewedMessage(videoPath) {
+  console.log(count++);
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
 
-app.get("/video", async (req, res) => {
-  const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+  const requestBody = {
+    videoPath,
+  };
 
-  const stats = await fs.promises.stat(videoPath);
+  const req = http.request("http://history/viewed", postOptions);
 
-  res.writeHead(200, {
-    "Content-Length": stats.size,
-    "Content-Type": "video/mp4",
+  req.on("end", () => {
+    console.log("Sent `viewed` message to history microservice");
   });
 
-  fs.createReadStream(videoPath).pipe(res);
-});
+  req.on("error", (err) => {
+    console.error("Failed to send 'viewed' message!");
+    console.error((err && err.stack) || err);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} v2 update.`);
+  req.write(JSON.stringify(requestBody));
+  req.end();
+}
+
+async function main() {
+  const app = express();
+
+  app.get("/video", async (req, res) => {
+    console.log("get /video");
+    // Route for streaming video.
+
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+
+    fs.createReadStream(videoPath).pipe(res);
+
+    sendViewedMessage(videoPath); // Sends the "viewed" message to indicate this video has been watched.
+  });
+
+  app.listen(PORT, () => {
+    console.log("Microservice online.");
+  });
+}
+
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
 });
